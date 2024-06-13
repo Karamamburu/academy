fbed836e459bdf64d1422ab91950a936227e17ff
Денис Хмыров <denis@Deniss-MacBook-Pro.local>
2024-06-13 22:29:10 +0300

    feat: all large components rendered

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
 import HeaderComponent from './components/HeaderComponent'
+import MainComponent from './components/MainComponent'
+import FooterComponent from './components/FooterComponent'
 
 function App() {
 
   return (
     <>
       <HeaderComponent />
+      <MainComponent />
+      <FooterComponent />
     </>
   )
 }
---
+++ src/components/FooterComponent.tsx
@@ -0,0 +1,33 @@
+function FooterComponent() {
+
+    return (
+        <footer className="footer">
+        <div className="footer-item-acdemy">
+          <h2>Академия.</h2>
+          <p>Целимся в Луну</p>
+        </div>
+        <div className="footer-item">
+          <h3>Карта сайта</h3>
+          <ul>
+            <a href="#0">Библиотека стандартов</a>
+            <a href="#0">Моё обучение</a>
+            <a href="#0">Команда</a>
+            <a href="#0">Цели и развитие</a>
+            <a href="#0">Помощь</a>
+          </ul>
+        </div>
+        <div className="footer-item">
+          <h3>Полезное</h3>
+          <ul>
+            <a href="#0">SkillsHub</a>
+            <a href="#0">Compliance club</a>
+            <a href="#0">ЧикенФМ</a>
+            <a href="#0">GSD</a>
+            <a href="#0">О компании</a>
+          </ul>
+        </div>
+      </footer>
+    )
+}
+
+export default FooterComponent
